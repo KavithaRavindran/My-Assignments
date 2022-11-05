85358b010e20d376c1745ad89a1308315a1e2a29
KavithaRavindran <117538292+KavithaRavindran@users.noreply.github.com>
2022-11-06 02:20:22 +0530

    Add files via upload

---
+++ Mobile1.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Mobile1 {
+	
+	 String mobileBrandName = "RoyalEnfield";
+	 char mobileLogo = 'B';
+	 short noOfMobilePiece = 5 ;
+	 int modelNumber = 5 ;
+	 long mobilemeiNumber = 5 ;
+	 float mobilePrice = 50000.55f;
+	 boolean isDamaged = true;
+	 
+	  		 
+
+	public static void main(String[] args) {
+		
+		Mobile1 A = new Mobile1 ();
+		System.out.println (A.mobileBrandName);
+		System.out.println(A.mobileLogo);
+		System.out.println (A.mobilemeiNumber);
+		System.out.println (A.mobilePrice);
+		System.out.println (A.isDamaged);
+	}
+}
+		
+	
+
+
