8356d7d422874b827c02c22c367ac3a6ada83df9
KavithaRavindran <117538292+KavithaRavindran@users.noreply.github.com>
2022-11-06 02:03:51 +0530

    Add files via upload
    Hi Dilip bro, please find day 1_assignment1 as attached for your review. 
    
    Thanks,
    Kavitha.R

---
+++ TwoWheeler.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class TwoWheeler {
+	
+	int noOfWheels = 2;
+	short noOfMirrors = 2;
+	long chasisNumber = 1122334455;
+	boolean isPunctured = true;
+	String bikeName1 = "Yamaha";
+	double runningKM = 1000.60;
+		
+
+	public static void main(String[] args) {
+		
+		TwoWheeler A = new TwoWheeler();
+		System.out.println (A.noOfWheels);
+		System.out.println (A.noOfMirrors);
+		System.out.println (A.chasisNumber);
+		System.out.println (A.isPunctured);
+		System.out.println (A.bikeName1);
+		System.out.println (A.runningKM);
+		// TODO Auto-generated method stub
+
+	}
+
+}
