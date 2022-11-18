20968d149ffe3dd7f6b6748754a75a8694806aac
KavithaRavindran <117538292+KavithaRavindran@users.noreply.github.com>
2022-11-18 12:17:16 +0530

    Add files via upload

---
+++ DeleteLead.java
@@ -0,0 +1,30 @@
+package Week2.Day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DeleteLead {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.linkText("Phone")).click();
+		driver.findElement(By.name("phoneNumber")).sendKeys("1122334455");
+		
+				
+	}
+
+}
