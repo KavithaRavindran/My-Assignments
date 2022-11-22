a8490d8e03822fb5c193e22b45c83bfbafaf7720
KavithaRavindran <117538292+KavithaRavindran@users.noreply.github.com>
2022-11-22 23:34:18 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,21 @@
+package Week3.Day1;
+
+public class BankInfo 
+
+{
+	public void saving()
+	
+	{
+		System.out.println("Saving's account");
+	}
+	public void fixed()
+	
+	{
+		System.out.println("Provides high interest");
+	}
+	public void deposit()
+	
+	{
+		System.out.println("Recurring deposit");
+	}
+}
